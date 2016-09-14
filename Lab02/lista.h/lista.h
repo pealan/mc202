ec512b1a9517af3e2ef05ae36f18ee1feecc812d
Pedro Alan <pealan97@gmail.com>
2016-09-14 10:22:54 -0300

    Create lista.h

---
+++ Lab02/lista.h/lista.h
@@ -0,0 +1,20 @@
+//Estrutura da lista duplamente encadeada
+typedef struct No{
+	int valor;
+	struct No *prox;
+	struct No *antes;
+}No;
+
+int addLista(No **inicioLista, int valor, int tipo);
+
+No *copiaLista(No *original);
+
+int acessaNo(No **inicioLista,int valor, int tipo);
+
+int removeNo(No **inicioLista,int valor);
+
+void imprimeLista(No *inicioLista);
+
+void imprimeListaR(No *inicioLista);
+
+void destroiLista(No **inicioLista);
